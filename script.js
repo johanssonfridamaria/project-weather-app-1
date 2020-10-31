69fd87c956614690f133d9bdcda4e5a759514dba
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-10-31 11:16:16 +0100

    fetched weather api, stored data in weather object

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const loc = document.querySelector('#location')
+let weather =[
+    {
 
-function getLocation(){
-    if(navigator.geolocation){
-       navigator.geolocation.getCurrentPosition(showPosition);
-    }else{
+    }
+]
+
+function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=b8b4c63b8481846f4a3bc15b450f2654`)
+    .then(res => res.json())
+    .then(data => {
+weather = data;
+console.log(weather)
+console.log(weather.main.temp)
+
+    })
+}
+
+function error() {
+    console.log('fail')
+}
+
+function getLocation() {
+    if (!navigator.geolocation) {
         console.log('error');
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
     }
 }
 
-function showPosition(position){
-loc.innerHTML = 'latitude:' + position.coords.latitude + 'longitude: ' + position.coords.longitude;
-}
-
-
 getLocation();
 
-
-
-// fetch('api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}')
