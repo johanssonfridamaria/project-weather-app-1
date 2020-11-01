7935eb7b28b07b931f080545cd2c07b105a30092
Frida Johansson <70971884+johanssonfridamaria@users.noreply.github.com>
2020-11-01 23:02:57 +0100

    added table w weather data

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const today = document.querySelector('#currentCast');
 const sunrise = document.querySelector('#sunrise');
 const sunset = document.querySelector('#sunset');
 const message = document.querySelector('#message');
+const img = document.querySelector('#img');
+const days = document.querySelector('#days');
 
 let weatherData = [];
 
@@ -66,29 +68,63 @@ function displayResult(data) {
     function messageContent(todaysWeather) {
 
         if (todaysWeather === 'Clear') {
-            message.textContent = `Get your sunnies on. ${city} is looking rather great today.`
+            img.innerHTML = `<img src="/Designs/Design-2/icons/noun_Sunglasses_2055147.svg" alt="Sunglasses"</img>`;
+            img.style.color='#F47775';
+            message.textContent = `Get your sunnies on. ${city} is looking rather great today.`;
         }
         else if (todaysWeather === 'Rain') {
-            message.textContent = `Don’t forget your umbrella. It’s wet in ${city} today.`
+            img.innerHTML = `<img src="/Designs/Design-2/icons/noun_Umbrella_2030530.svg" alt="Umbrella"></img>`;
+            img.style.color='#164A68';
+            message.textContent = `Don’t forget your umbrella. It’s wet in ${city} today.`;
         }
         else if (todaysWeather === 'Clouds') {
-            message.textContent = `Light a fire and get cosy. ${city} is looking grey today.`
+            img.innerHTML = `<img src="/Designs/Design-2/icons/noun_Cloud_1188486.svg" alt="Cloud"</img>`;
+            img.style.color='#F47775';
+            message.textContent = `Light a fire and get cosy. ${city} is looking grey today.`;
         }
         else if (todaysWeather === 'Snow') {
-            message.textContent = `Don't forget your hat and gloves today. ${city} is snowy today.`
+            img.innerHTML = `<i class="far fa-snowflake"></i>`;
+            img.style.fontSize='4rem';
+            img.style.color='#164A68';
+            message.textContent = `Don't forget your hat and gloves today. ${city} is snowy today.`;
         }
     }
 
     messageContent(todaysWeather);
 
     let dailyWeather = [];
-    dailyWeather= data.daily;
+    dailyWeather = data.daily;
     console.log(dailyWeather)
 
+    for (i = 1; i < dailyWeather.length; i++) {
+        let date = dailyWeather[i].dt;
+        date = new Date(date * 1000).toLocaleString("eng", { weekday: "long" });
+        let minTemp = dailyWeather[i].temp.min;
+        let maxTemp = dailyWeather[i].temp.max;
+        let weather = dailyWeather[i].weather[0].main;
+        let iconId = dailyWeather[i].weather[0].icon;
 
-    for(i=0; i< dailyWeather.length; i++){
-        let date= dailyWeather[i].dt;
-        date = new Date(date * 1000).toDateString();
-        console.log(date);
+        let day = {
+            date: date,
+            minTemp: minTemp,
+            maxTemp: maxTemp,
+            weather: weather,
+            iconId: iconId
+        }
+
+            const tr = document.createElement('tr')
+            const weekday = document.createElement('td')
+            const temp = document.createElement('td')
+            const icon = document.createElement('td')
+    
+            weekday.innerText=`${day.date}`;
+            temp.innerText=`${day.minTemp}°/${day.maxTemp}°C`;
+            icon.innerHTML=`<img src=http://openweathermap.org/img/wn/${day.iconId}@2x.png alt="WeatherIcon"</img>`;
+    
+            tr.appendChild(weekday);
+            tr.appendChild(temp);
+            tr.appendChild(icon);
+            days.appendChild(tr);
+
     }
 }
